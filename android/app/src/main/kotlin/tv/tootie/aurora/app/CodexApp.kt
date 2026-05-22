cf3c7ef9a24fa3a29894fc23a841b6a1934cd57a
Jacob Magar <jmagar@gmail.com>
2026-05-22 18:22:36 -0400

    feat(android): expose CodexRepository singleton from CodexApp

--- android/app/src/main/kotlin/tv/tootie/aurora/app/CodexApp.kt
+++ android/app/src/main/kotlin/tv/tootie/aurora/app/CodexApp.kt
@@ -1,5 +1,13 @@
 package tv.tootie.aurora.app
 
 import android.app.Application
+import tv.tootie.aurora.app.codex.CodexRepository
 
-class CodexApp : Application()
+class CodexApp : Application() {
+    val repository: CodexRepository by lazy { CodexRepository() }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        repository.disconnect()
+    }
+}
